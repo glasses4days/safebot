d1ba5ab752d3f3068a1bf1be4ea5dbe61176454e
glasses4days <cooper@Coopers-MacBook-Air.local>
2016-07-23 19:05:19 -0700

    Add geolocation to helper function #workinprogress

--- twilio_api.py
+++ twilio_api.py
@@ -1,8 +1,9 @@
 from twilio.rest import TwilioRestClient
 import os
+from sqlalchemy import func
 
 
-def send_unsafe_text():
+def send_unsafe_text(user_id):
 
     # get users friend info and send the text that user is feeling unsafe
     # along with their geolocation 
@@ -11,7 +12,9 @@ def send_unsafe_text():
     sender = os.environ["SENDER"]
     friends = Friend.query.filter_by(user_id=user_id).all()
     contact_numbers = friends.contact_numbers
-  
+    unsafe_instances = Unsafe.query.filter_by(user_id=user_id).all()
+    max_unsafe_instance = db.session.query(func.max(Unsafe.unsafe_id)).one()
+    location = Unsafe.query.filter_by(max_unsafe_instance)
 
     for num in contact_numbers:
         recipient = num
